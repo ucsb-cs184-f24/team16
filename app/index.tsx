9af79f2fb75b01224d8c87b37f4b087c0b943f75
Boming Kong <michaelkongboming@gmail.com>
2024-10-23 16:37:26 -0700

    Remove unneeded calendar

--- app/index.tsx
+++ app/index.tsx
@@ -21,43 +21,6 @@ export default function Index() {
     setStudentId(data.id);
   });
 
-  useUCSBAuth("/", async headers => {
-    const response = await fetch("https://api-transformer.onrender.com//https://my.sa.ucsb.edu/gold/WeeklyStudentSchedule.aspx", {
-      "method": "GET",
-      "headers": headers
-    });
-    const dom = jsdom(await response.text());
-    console.log("Dom", dom.title);
-    const eventsElement = dom.querySelector('#pageContent_events');
-    if (eventsElement) {
-      const events: {[p: string]: {
-        start: string | undefined;
-        end: string | undefined;
-        content: string | undefined;
-        event: string | undefined;
-      }[]} = {};
-      for (const [day, selector] of Object.entries({
-        M: '#pageContent_eventsgroupM',
-        T: '#pageContent_eventsgroupT',
-        W: '#pageContent_eventsgroupW',
-        R: '#pageContent_eventsgroupR',
-        F: '#pageContent_eventsgroupF'
-      })) {
-        const eventGroup = eventsElement.querySelector(selector);
-        if (eventGroup) {
-          const currentEvents = eventGroup.querySelectorAll('.single-event');
-          events[day] = Array.from(currentEvents).map((eventElement: Element) => ({
-            start: eventElement.attributes.getNamedItem('data-start')?.value,
-            end: eventElement.attributes.getNamedItem('data-end')?.value,
-            content: eventElement.attributes.getNamedItem('data-content')?.value,
-            event: eventElement.attributes.getNamedItem('data-event')?.value
-          }));
-        }
-      }
-      Alert.alert("UCSB Schedule", JSON.stringify(events));
-    }
-  });
-
   return (
       <View
           style={{
