5e06d2f1214fe0ceb374dda68bdae1f2c596a587
Boming Kong <michaelkongboming@gmail.com>
2024-11-03 15:44:58 -0800

    Add new page for Quarter information

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
-import {View} from 'react-native';
+import {View, Button, TouchableOpacity, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Icon } from 'native-base';
 import Schedule from '@/components/Schedule';
 import {useCanvasAuth} from '@/app/canvas-auth';
 import {useUCSBAuth} from "@/app/ucsb-auth";
 import {getCanvasEvents, getQuarter, getUCSBEvents, Quarter, UCSBEvents} from '@/helpers/api';
 import {useRef, useState} from "react";
 import {Mutex} from "async-mutex";
+import {router} from "expo-router"
+import {quarter_screen} from "./quarter-screen"
 
 export default function Index() {
   const [quarter, setQuarter] = useState<Quarter | null>(null);
@@ -71,18 +76,29 @@ export default function Index() {
 //   }
 
   return (
-      <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-      >
+      <View style={styles.container} >
+
+        <Button
+                        title="Check My Quarter"
+                        onPress={() => router.navigate('quarter-screen')}
+                      />
         <Schedule
-            quarter={quarter}
-            canvasEvents={canvasEvents}
-            ucsbEvents={UCSBEvents}
-        />
+                    quarter={quarter}
+                    canvasEvents={canvasEvents}
+                    ucsbEvents={UCSBEvents}
+                />
+
       </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
