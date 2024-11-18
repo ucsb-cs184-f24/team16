15b12076eae442f2c0ba13e6a48c4e9791d17dc1
marcushuang1 <108109784+marcushuang1@users.noreply.github.com>
2024-11-18 14:41:28 -0800

    Hide the Header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import {Stack} from "expo-router";
 export default function RootLayout() {
   return (
       <Stack>
-        <Stack.Screen name="index"/>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="canvas-auth"/>
         <Stack.Screen name="ucsb-auth"/>
         <Stack.Screen name="quarter-screen"/>
